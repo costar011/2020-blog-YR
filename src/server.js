6268f7923a6f79853b2ad1dab7055109490ae0b1
YERIM <dnaer456@gmail.com>
2020-11-29 19:22:33 +0900

    end

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import morgan from "morgan";
+import globalRouter from "./router/globalRouer";
 
 const PORT = process.env.PORT;
 
@@ -10,6 +11,9 @@ const app = express();
 app.set("view engine", "pug");
 
 app.use(morgan(`dev`));
+
+app.get("/", globalRouter);
+app.get("/contact", globalRouter);
 
 app.listen(PORT, () => {
   console.log(`✅ Server Start ${PORT}`);
